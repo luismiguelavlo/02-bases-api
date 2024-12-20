55921b11a34acb6f8f8aa20f204f21969d003cd7
Luis Miguel <redes.teleco.lm@gmail.com>
2024-12-19 21:00:34 -0500

    endpoints definidos

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,35 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { assignToy, createToy, packedToy, seeWorkshop } from "./routes";
 
 const app = express(); //? con esta linea inicializamos express
 
 app.use(express.json()); //? Enseñarle a express a que entienda informacion que venga en formato json
 
-app.listen(3000, () => {
-  console.log("app running on port 3000");
+interface Toy {
+  id: number;
+  name: string;
+  type: string;
+  magicLevel: number;
+  packed: boolean;
+  assignedTo: string | null;
+}
+
+const toys: Toy[] = [];
+let toyId = 1;
+
+//crear el juguete
+app.post("/toy", createToy);
+
+//empacar el juguete //* :id es el id del juguete que quiero empacar
+app.get("/toy/:id/pack", packedToy);
+
+//asignar juguete a un niño
+app.post("/toy/:id/assign", assignToy);
+
+//ver el estado del taller
+app.get("/workshop", seeWorkshop);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
 });
---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+export const createToy = (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+  });
+};
+
+export const packedToy = (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+  });
+};
+
+export const assignToy = (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+  });
+};
+
+export const seeWorkshop = (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+  });
+};
